041447c04599a05b01762824b0d680880fd8788e
tangtyk <tyklww@163.com>
2017-03-28 17:18:32 +0800

    Add files via upload

--- v1.1_source/attFrame.py
+++ v1.1_source/attFrame.py
@@ -6,11 +6,17 @@ ID_ABOUT = 200
 class ATTFrame(wx.Frame):
 
 	def __init__(self, parent = None, title = None):
-		wx.Frame.__init__(self, parent, -1, 'ATT Automation Tool', size = (600, 300), style = wx.DEFAULT_FRAME_STYLE|wx.STAY_ON_TOP)
+		"""v1.2 begin"""
+		# wx.Frame.__init__(self, parent, -1, 'ATT Automation Tool', size = (600, 300), style = wx.DEFAULT_FRAME_STYLE|wx.STAY_ON_TOP)
+		wx.Frame.__init__(self, parent, -1, 'Automate Statistics', size = (600, 300), style = wx.DEFAULT_FRAME_STYLE|wx.STAY_ON_TOP)
+		"""v1.2 end"""
 		self.SetMaxSize((600, 300))
 		self.SetMinSize((600,300))
 		self.Center()
-		self.icon = wx.Icon('ATTtool.ico', wx.BITMAP_TYPE_ICO)
+		"""v1.2 begin"""
+		# self.icon = wx.Icon('ATTtool.ico', wx.BITMAP_TYPE_ICO)
+		self.icon = wx.Icon('Automate Statistics.ico', wx.BITMAP_TYPE_ICO)
+		"""v1.2 end"""
 		self.SetIcon(self.icon)
 		self.menuBar = wx.MenuBar()
 		self.menu = wx.Menu()
@@ -23,10 +29,10 @@ class ATTFrame(wx.Frame):
 		self.statusbar.SetStatusText('IBM Confidential', 1)
 
 	def onAbout(self, evt):
-		dlg = wx.MessageDialog(self, 'ATT Automation Tool\n© Copyright IBM Corporation 2017\nAll Rights Reserved\nVersion: 1.1, powered by SPRT team\nContact us:\nlizhiwcd@cn.ibm.com\ntyktang@cn.ibm.com', 'About ATT Automation Tool', wx.OK)
+		dlg = wx.MessageDialog(self, 'Automate Statistics\n© Copyright IBM Corporation 2017\nAll Rights Reserved\nVersion: 1.1, powered by SPRT team\nContact us:\nlizhiwcd@cn.ibm.com\ntyktang@cn.ibm.com', 'About Automate Statistics', wx.OK)
 		dlg.ShowModal()
 		dlg.Destroy()
-		# description = 'ATT Automation Tool is an internal tool to generate/get report from site:Agile Team Tool\nVersion: 1.0, powered by SPRT team\nContact us:\nlizhiwcd@cn.ibm.com\ntyktang@cn.ibm.com'
+		# description = 'Automate Statistics is an internal tool to generate/get report from site:Agile Team Tool\nVersion: 1.0, powered by SPRT team\nContact us:\nlizhiwcd@cn.ibm.com\ntyktang@cn.ibm.com'
 		# info = wx.AboutDialogInfo()
 		# info.SetIcon(wx.Icon('superman_48px.ico',wx.BITMAP_TYPE_PNG))
 		# info.SetDescription(description)
